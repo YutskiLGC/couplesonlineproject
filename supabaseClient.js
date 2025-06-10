075cb34580c23260e98e6eb7aceb86774f3fc645
Brandon Bozeman <128614590+YutskiLGC@users.noreply.github.com>
2025-06-10 00:17:40 -0400

    doesnt want to launch

---
+++ supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient('https://fhtcqjecauijtftifnid.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZodGNxamVjYXVpanRmdGlmbmlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyNzk1MjIsImV4cCI6MjA2MDg1NTUyMn0.9xiFe8Q3ls4_CK_PuTyc3du0ZW6Cab5VybPhYJVeAr8');
